eca705b001d2e2b777dd0678c53c5ba569b7c828
david vadnais <dvadnais@localhost.localdomain>
2020-07-15 16:12:23 -0600

    validation in network script

--- delayNetwork.sh
+++ delayNetwork.sh
@@ -2,8 +2,10 @@
 
 # Used to solve s9 error for league of legends on centos8
 
+[[ -z "${1}" ]] && echo "I cant work without an input of on or off" && exit 1
+
+
 onOrOff=${1}
-echo ${onOrOff}
 
 if [ "${onOrOff}" = "on" ]
 then
@@ -12,6 +14,8 @@ then
 elif [ "${onOrOff}" = "off" ]; then
 	tc qdisc del dev enp5s0 root
 	echo "delay off"
+else 
+	echo "only accepts inputs of on or off" && exit 1
 fi
 
 exit 0 
