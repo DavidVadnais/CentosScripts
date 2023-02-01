5b0df40708deabde3f0f791218f77db0cbcfd0de
David Vadnais <56882532+DavidVadnais@users.noreply.github.com>
2023-01-31 18:25:43 -0700

    Create dockerPastBin.sh

---
+++ docker/dockerPastBin.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+# Purpose: copy paste bin for docker commands.
+
+# export the container id for use later.
+export LATEST_CONTAINER=\
+  $(docker run -d -p 5001:5001  \
+  -p 8080:8080 \
+  -e SERVICE_EMAIL=david.vadnaiscs@gmail.com \
+  -e HOSTNAME_BE=biomlearn.uccs.edu \
+  -e SERVICE_EMAIL_KEY=${KEY} \
+  particlechromo3d:latest) \
+  && docker exec -it ${LATEST_CONTAINER} /bin/bash
