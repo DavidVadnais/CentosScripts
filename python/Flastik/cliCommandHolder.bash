3a02af516a5b7666a3f35fa51b59102a98975d4a
David Vadnais <56882532+DavidVadnais@users.noreply.github.com>
2023-08-24 22:29:50 +0000

    Update cliCommandHolder.bash

--- python/Flastik/cliCommandHolder.bash
+++ python/Flastik/cliCommandHolder.bash
@@ -6,4 +6,10 @@ python flastiktastic.py
 
 
 ### falkoor 2
+# build missing netcdf
 python ./falkor_website.py --make_netcd
+#rebuild all netcdf
+python ./falkor_website.py --make_netcdf --data_dir="../falkor_data" --force
+
+### Hot
+python ./hot_cruises_website.py --make_netcdf --data_dir="../../data" --force
